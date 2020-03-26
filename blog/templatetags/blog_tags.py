826c8f3a83eaa76a5795e16c4a87678a9a5ee0ed
dima <zenandmin@gmail.com>
2020-03-27 00:52:38 +0300

    added tags, filter, markdown

---
+++ blog/templatetags/blog_tags.py
@@ -0,0 +1,30 @@
+from django import template
+from django.db.models import Count, Q
+from ..models import Post
+from django.utils.safestring import mark_safe
+import markdown
+
+register = template.Library()
+
+
+@register.simple_tag
+def total_posts():
+    return Post.published.count()
+
+
+@register.inclusion_tag('blog/post/latest_posts.html')
+def show_latest_posts(count=5):
+    latest_posts = Post.published.order_by('-publish')[:count]
+    return {'latest_posts': latest_posts}
+
+
+@register.simple_tag
+def get_most_commented_posts(count=5):
+    return Post.published.annotate(
+        cnt_comments=Count('comments', filter=Q(comments__active=True))
+        ).order_by('-cnt_comments', '-publish')[:count]
+
+
+@register.filter(name='markdown')
+def markdown_format(text):
+    return mark_safe(markdown.markdown(text))
