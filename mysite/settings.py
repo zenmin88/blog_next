a232b8c75f10e79321c0d0027434c97b5f2f575e
dima <zenandmin@gmail.com>
2020-03-27 05:01:54 +0300

    Change database to postgresql

--- mysite/settings.py
+++ mysite/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     # 3rd Part
     'django_extensions',
     'taggit',
+    'django.contrib.postgres',
 
     # Local
     'users.apps.UsersConfig',
@@ -92,10 +93,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': env.db()
 }
 
 
